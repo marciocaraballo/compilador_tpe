f1497361bd3487392c249b8afae9a90a6694b9a2
Marcio Caraballo <>
2022-09-15 19:28:38 +0000

    Testing

---
+++ src/compilador/Parser.java
@@ -0,0 +1,10 @@
+package compilador;
+
+public class Parser {
+  public static void main(String[] args) { 
+    
+    String archivo_a_leer = args[0];
+
+    System.out.println("Se va a leer archivo " + archivo_a_leer);
+  }
+}
