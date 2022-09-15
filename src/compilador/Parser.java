ca202487c386733981c810aa385ed8fdac5d48b4
Marcio Caraballo <marcio.caraballososa@gmail.com>
2022-09-15 17:02:48 -0300

    Testing

--- src/compilador/Parser.java
+++ src/compilador/Parser.java
@@ -1,25 +1,34 @@
 package compilador;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class Parser {
   public static void main(String[] args) { 
     
-    String archivo_a_leer = args[0];
-    //String delim = "/n";
+    String archivo_a_leer = args[0]; 
 
     System.out.println("Se va a leer archivo " + archivo_a_leer);
 
-    BufferedReader reader = new BufferedReader(new FileReader(archivo_a_leer));
+    BufferedReader reader = null;
+	try {
+		reader = new BufferedReader(new FileReader(archivo_a_leer));
+	} catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 
-    int firstChar = reader.read();
+    int firstChar = 0;
+	try {
+		firstChar = reader.read();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 
-    System.out.println(firstChar);
-    
-    // String[] result = archivo_a_leer.split(delim);
-    // for (String resultvalue : result) {
-    //   System.out.println(resultvalue);
+    	System.out.println(firstChar);
     }
   
 }
