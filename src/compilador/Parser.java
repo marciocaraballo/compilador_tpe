134871307e3335dffd2ca2dc861512943671bfc4
Marcio Caraballo <marcio.caraballososa@gmail.com>
2022-09-15 19:44:51 +0000

    Test

--- src/compilador/Parser.java
+++ src/compilador/Parser.java
@@ -4,7 +4,12 @@ public class Parser {
   public static void main(String[] args) { 
     
     String archivo_a_leer = args[0];
+    String delim = "/n";
 
     System.out.println("Se va a leer archivo " + archivo_a_leer);
+    String[] result = archivo_a_leer.split(delim);
+    for (String resultvalue : result) {
+      System.out.println(resultvalue);
+    }
   }
 }
