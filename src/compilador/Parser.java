cf808f44f28216c080b50445726e943545ceb273
Marcio Caraballo <marcio.caraballososa@gmail.com>
2022-09-15 20:00:08 +0000

    test

--- src/compilador/Parser.java
+++ src/compilador/Parser.java
@@ -1,15 +1,25 @@
 package compilador;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Parser {
   public static void main(String[] args) { 
     
     String archivo_a_leer = args[0];
-    String delim = "/n";
+    //String delim = "/n";
 
     System.out.println("Se va a leer archivo " + archivo_a_leer);
-    String[] result = archivo_a_leer.split(delim);
-    for (String resultvalue : result) {
-      System.out.println(resultvalue);
+
+    BufferedReader reader = new BufferedReader(new FileReader(archivo_a_leer));
+
+    int firstChar = reader.read();
+
+    System.out.println(firstChar);
+    
+    // String[] result = archivo_a_leer.split(delim);
+    // for (String resultvalue : result) {
+    //   System.out.println(resultvalue);
     }
-  }
+  
 }
