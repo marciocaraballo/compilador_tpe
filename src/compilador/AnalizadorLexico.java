1733dc076e4d1454db84c328a752d1f83f720e68
Marcio Caraballo <marcio.caraballososa@gmail.com>
2022-09-15 22:54:58 -0300

    Stuff

--- src/compilador/AnalizadorLexico.java
+++ src/compilador/AnalizadorLexico.java
@@ -1,12 +1,23 @@
 package compilador;
 
+import java.io.BufferedReader;
+
 public class AnalizadorLexico {
 	
 	private MatrixEstados matrixEstados = new MatrixEstados();
 	
+	/** Se asume que se inicia en el estado 0 */
 	private int estado_actual = 0;
+	/** Acumula el token leido hasta el momento */
+	private String token_actual = new String(" ");
+	/** Mantiene la linea del programa leia hasta el momento */
+	private int linea_actual = 0;
+	/** Referencia al Reader de archivo de codigo */
+	private BufferedReader lector_archivo = null;
 	
-	public AnalizadorLexico() {};
+	public AnalizadorLexico(BufferedReader lector) {
+		lector_archivo = lector;
+	};
 	
 	public int getToken() {
 		
