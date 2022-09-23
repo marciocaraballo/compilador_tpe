6c8b35ef02b4e55a217af45ba021eeeec0c360ba
Marcio Caraballo <marcio.caraballososa@gmail.com>
2022-09-23 20:03:35 -0300

    Remueve comments

--- src/compilador/MatrixAccionesSemanticas.java
+++ src/compilador/MatrixAccionesSemanticas.java
@@ -4,25 +4,7 @@ import accion_semantica.*;
 
 public class MatrixAccionesSemanticas {
 	
-//	private AS1 AS1 = new AS1();
-//	private AS2 AS2 = new AS2();
-//	private AS3 AS3 = new AS3();
-//	private AS4 AS4 = new AS4();
-//	private AS5 AS5 = new AS5();
-//	private AS6 AS6 = new AS6();
-//	private AS7 AS7 = new AS7();
-//	private AS8 AS8 = new AS8();
-//	private AS9 AS9 = new AS9();
-//	private ASE ASE = new ASE();
-	
-//	private AccionSemantica[][] matrixAccionesSemanticas = {
-//			   /*L*/ /*l*/ /*d*/ /*"D"*/ /*“_”*/ /*"+"*/ /*"-"*/ /*"{"*/ /*"}"*/ /*"("*/ /*")"*/ /*";"*/ /*"/"*/ /*"*"*/ /*","*/ /*"="*/ /*">"*/ /*"<"*/ /*":"*/ /*"!"*/ /*"."*/ /*"'"*/ /*tab*/ /*bl*/ /*nl*/ /*"$"*/
-//		/* 0*/ {AS2,  AS2,  AS2,    AS2,   null,    AS1,    AS1,    AS1,    AS1,    AS1,    AS1,    AS1,    AS1,    AS1,    AS1,    AS2,    AS2,    AS2,    ASE,    ASE,    AS2,    AS2,    AS9,   AS9,   AS9,   null},
-//		/* 1*/ {AS3,  AS3,  AS3,    AS3,   null,    AS4,    AS4,    AS4,    AS4,    AS4,    AS4,    AS4,    AS4,    AS4,    AS4,    AS4,    AS4,    AS4,    AS4,    AS4,    AS4,    AS4,    AS4,   AS4,   AS4,   null},
-//		/* 2*/ {AS4,  AS4,  AS3,    AS5,   null,    AS5,    AS5,    AS5,    AS5,    AS5,    AS5,    AS5,    AS5,    AS5,    AS5,    AS5,    AS5,    AS5,    AS5,    AS5,    AS5,    AS5,    AS5,   AS5,   AS5,   null}
-//	};
-	
-	private AccionSemantica[][] matrixAccionesSemanticas = new AccionSemantica[25][25];
+	private AccionSemantica[][] matrixAccionesSemanticas = new AccionSemantica[14][25];
 
 	public MatrixAccionesSemanticas(TablaDeSimbolos ts, TablaPalabrasReservadas tpr) {
 		
@@ -59,7 +41,7 @@ public class MatrixAccionesSemanticas {
 	* 
 	* @param initialState 
 	* @param column
-	* @return
+	* @return AccionSemantica
 	*/
 	public AccionSemantica getAccionSemantica(int initialState, int column) {
 		return this.matrixAccionesSemanticas[initialState][column];
