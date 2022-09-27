9742384a3cc7af34b638e7f7b778a3b76cd966a7
Marcio Caraballo <marcio.caraballososa@gmail.com>
2022-09-27 01:00:56 -0300

    fixes

--- src/accion_semantica/AS7.java
+++ src/accion_semantica/AS7.java
@@ -18,6 +18,7 @@ public class AS7 extends AccionSemantica {
 
 	@Override
 	public int ejecutar(FileReaderHelper fileHelper, StringBuilder lexema, char nextCharacter) {	
+		fileHelper.reset();
 		return (int) lexema.charAt(0);
 	}
 }
