e48081846a498ccd2ba7075e249481d61b520f40
Marcio Caraballo <marcio.caraballososa@gmail.com>
2022-10-23 00:46:43 -0300

    Fix AS4

--- src/accion_semantica/AS4.java
+++ src/accion_semantica/AS4.java
@@ -5,22 +5,6 @@ import compilador.Logger;
 import compilador.TablaDeSimbolos;
 import compilador.TablaPalabrasReservadas;
 
-/**
- * -Devolver a la entrada el último carácter leído 
-	-Verificar tamaño de cadena
--Buscar en la TPR: 
- - Si está, devolver token de PR 
- - Si no está: 
--Buscar en la TS:
- -Si está, devolver ID + Punt TS 
- -Si no está: 
--Alta en TS 
--Devolver ID + Punt TS
-
- *
- *	ID es token 100
- *
- */
 public class AS4 extends AccionSemantica {
 
 	public AS4(TablaPalabrasReservadas TPR, TablaDeSimbolos TS, Logger logger) {
@@ -30,18 +14,17 @@ public class AS4 extends AccionSemantica {
 	@Override
 	public int ejecutar(FileReaderHelper fileHelper, StringBuilder lexema, char nextCharacter) {
 		
-		if (lexema.length() > 25) {
-			logger.logWarning("Has superado la cantidad maxima de caracteres para un identificador(25), se eliminaran los"
-					+  " caracteres que estan mas alla de la posicion 25 ");
-			lexema.setLength(25);
-		}
-		
-		//Devuelve el input a la entrada
 		fileHelper.reset();
 		
 		if (TPR.getToken(lexema.toString()) != -1) {
 			return TPR.getToken(lexema.toString());
 		} else {
+			if (lexema.length() > 25) {
+				logger.logWarning("Se ha superado la cantidad maxima de caracteres para un identificador(25), se eliminaran los"
+						+  " caracteres que estan mas alla de la posicion 25 ");
+				lexema.setLength(25);
+			}
+			
 			if (TS.has(lexema.toString())) {
 				return TS.getToken(lexema.toString());
 			} else {
