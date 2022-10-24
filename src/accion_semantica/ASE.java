4bda8304e831ce6116e5530b714a1e29c8d1b6d4
Marcio Caraballo <marcio.caraballososa@gmail.com>
2022-10-23 23:39:12 -0300

    Fix punto

--- src/accion_semantica/ASE.java
+++ src/accion_semantica/ASE.java
@@ -7,9 +7,18 @@ public class ASE extends AccionSemantica {
 	@Override
 	public int ejecutar(FileReaderHelper fileHelper, StringBuilder lexema, char nextCharacter) {
 		
-		Logger logger = Logger.getInstance();
+		Logger logger = Logger.getInstance();	
+		/**
+		 * El . es un caso particular, hay error en caso de que nada 
+		 * que venga despues sea un numero. Si eso sucede, se cae al error
+		 * y hay que devolver el caracter leido a la entrada
+		 */
+		if (lexema.toString().equals(".")) {
+			fileHelper.reset();
+		} else {
+			lexema.append(nextCharacter);
+		}
 		
-		lexema.append(nextCharacter);
 		logger.logError("[Lexico] Lexema no reconocido: " + lexema.toString() + ", se descarta.");
 		lexema.setLength(0);
 		
