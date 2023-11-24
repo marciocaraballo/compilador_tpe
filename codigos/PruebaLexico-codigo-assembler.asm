021643b11466cbb9ba42e2d7ea9146e2a8e39213
Marcio Caraballo <marcio.caraballososa@gmail.com>
2023-11-24 15:48:39 -0300

    issues

--- codigos/PruebaLexico-codigo-assembler.asm
+++ codigos/PruebaLexico-codigo-assembler.asm
@@ -7,13 +7,7 @@ include \masm32\include\user32.inc
 includelib \masm32\lib\kernel32.lib
 includelib \masm32\lib\user32.lib
 .data
-@aux0 REAL4 ?
-@aux1 REAL4 ?
-@aux2 REAL4 ?
-a_main REAL4 ?
-variable_real_1 REAL4 -3.40282347E+38
 
-variable_real_0 REAL4 3.40282347E+38
 recursion_flag DW 0 
 error_recursividad db " No se admite recursividad de invocación a funciones " , 0
 overflow_flotantes db " La suma de los valores ha sobrepasado el rango " , 0
@@ -26,88 +20,36 @@ maximo_rango_positivo REAL4 3.40282347E+38
 aux_mem REAL4 ?
 .code
 ;-------------------------- ESTO ES PARA MEJORAR VISUALIZACION ----------------- 
+main_ca_m_n_z:
+CMP recursion_flag, 1
+JNE CONTINUAR_EJECUCION_MAIN_CA_M_N_Z 
+invoke MessageBox, NULL, addr error_recursividad, addr error_recursividad, MB_OK
+invoke ExitProcess, 0
+CONTINUAR_EJECUCION_MAIN_CA_M_N_Z: 
+MOV recursion_flag, 1
+MOV recursion_flag, 0
+RET
+;-------------------------- ESTO ES PARA MEJORAR VISUALIZACION ----------------- 
+main_ca_m:
+CMP recursion_flag, 2
+JNE CONTINUAR_EJECUCION_MAIN_CA_M 
+invoke MessageBox, NULL, addr error_recursividad, addr error_recursividad, MB_OK
+invoke ExitProcess, 0
+CONTINUAR_EJECUCION_MAIN_CA_M: 
+MOV recursion_flag, 2
+MOV recursion_flag, 0
+RET
+;-------------------------- ESTO ES PARA MEJORAR VISUALIZACION ----------------- 
 main:
-FLD variable_real_0
-FLD variable_real_1
-FADD 
-FSTP @aux0
-FLD @aux0
-FLD zero
-FCOMPP
-FSTSW AX
-SAHF
-JE FIN_SUMA_1
-FLD @aux0
-FLD maximo_rango_positivo
-FCOMPP
-FSTSW AX
-SAHF
-JB ERROR_SUMA_FLOTANTE_1
-FLD @aux0
-FLD minimo_rango_negativo
-FCOMPP
-FSTSW AX
-SAHF
-JA ERROR_SUMA_FLOTANTE_1
-FLD @aux0
-FLD maximo_rango_negativo
-FCOMPP
-FSTSW AX
-SAHF
-JA FIN_SUMA_1
-FLD @aux0
-FLD minimo_rango_positivo
-FCOMPP
-FSTSW AX
-SAHF
-JB FIN_SUMA_1
-ERROR_SUMA_FLOTANTE_1:
-invoke MessageBox, NULL, addr overflow_flotantes, addr overflow_flotantes, MB_OK
-invoke ExitProcess, 0
-FIN_SUMA_1:
-FLD variable_real_0
-FLD @aux0
-FADD 
-FSTP @aux1
-FLD @aux1
-FLD zero
-FCOMPP
-FSTSW AX
-SAHF
-JE FIN_SUMA_2
-FLD @aux1
-FLD maximo_rango_positivo
-FCOMPP
-FSTSW AX
-SAHF
-JB ERROR_SUMA_FLOTANTE_2
-FLD @aux1
-FLD minimo_rango_negativo
-FCOMPP
-FSTSW AX
-SAHF
-JA ERROR_SUMA_FLOTANTE_2
-FLD @aux1
-FLD maximo_rango_negativo
-FCOMPP
-FSTSW AX
-SAHF
-JA FIN_SUMA_2
-FLD @aux1
-FLD minimo_rango_positivo
-FCOMPP
-FSTSW AX
-SAHF
-JB FIN_SUMA_2
-ERROR_SUMA_FLOTANTE_2:
-invoke MessageBox, NULL, addr overflow_flotantes, addr overflow_flotantes, MB_OK
-invoke ExitProcess, 0
-FIN_SUMA_2:
-FLD variable_real_0
-FLD @aux1
-FSUB 
-FSTP @aux2
-FLD @aux2
-FSTP a_main
 invoke ExitProcess, 0
 end main
+;-------------------------- ESTO ES PARA MEJORAR VISUALIZACION ----------------- 
+main_ca_m_n:
+CMP recursion_flag, 3
+JNE CONTINUAR_EJECUCION_MAIN_CA_M_N 
+invoke MessageBox, NULL, addr error_recursividad, addr error_recursividad, MB_OK
+invoke ExitProcess, 0
+CONTINUAR_EJECUCION_MAIN_CA_M_N: 
+MOV recursion_flag, 3
+MOV recursion_flag, 0
+RET
